4942ec806f04e7f8afa5f4262f4cf20416a55b0d
meher <meherprasad720@gmail.com>
2019-08-19 02:58:04 +0530

    modified server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/bank-search-app'));
+app.use(express.static(__dirname,'/dist/bank-search-app'));
 
 app.get('/*', function(req,res) {
     
