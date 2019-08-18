c4811acd9b7917ed40eeae2879c1470971933f8c
meher <meherprasad720@gmail.com>
2019-08-19 02:43:42 +0530

    modified server.js

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/BANKSEARCHAPP'));
+app.use(express.static('./dist/bank-search-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/BANKSEARCHAPP/index.html'));
+res.sendFile(path.join(__dirname,'/dist/bank-search-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
